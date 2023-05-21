6781b453aa0e8fe0352192deecfa53ed3abfb6db
Vlad Kulish <vladkulish19@gmail.com>
2023-05-21 19:55:36 +0300

    most are done

--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -1,6 +1,18 @@
 package lang.print.gaps.finalModuleTask;
 
+import java.io.IOException;
+
 public class NumberReverter {
     public void revert(int number) {
+
+
+
+        int third = number%10;
+
+        int second = (number%100)/10;
+
+        int first =number/100;
+
+        System.out.println(third+""+second+""+first);
     }
 }
